d2f510e2074817a6a12dfca9d87fa9dece453e96
Rio Adrian <rioadriansky02@gmail.com>
2025-07-16 22:37:45 +0700

    initial commit

---
+++ src/api/notes/handler.js
@@ -0,0 +1,78 @@
+class NotesHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postNoteHandler = this.postNoteHandler.bind(this);
+    this.getNotesHandler = this.getNotesHandler.bind(this);
+    this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
+    this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
+    this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
+  }
+
+  async postNoteHandler(request, h) {
+    this._validator.validateNotePayload(request.payload);
+    const { title = 'untitled', body, tags } = request.payload;
+
+    const noteId = await this._service.addNote({ title, body, tags });
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async getNotesHandler(request, h) {
+    const notes = await this._service.getNotes();
+    const response = h.response({
+      status: 'Success',
+      data: {
+        notes,
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  async getNoteByIdHandler(request, h) {
+    const { id } = request.params;
+    const note = await this._service.getNoteById(id);
+
+    const response = h.response({
+      status: 'Success',
+      data: {
+        note,
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  async putNoteByIdHandler(request, h) {
+    this._validator.validateNotePayload(request.payload);
+    const { id } = request.params;
+    await this._service.editNoteById(id, request.payload);
+
+    return {
+      status: 'Succes',
+      message: 'Catatan berhasil diperbarui',
+    };
+  }
+
+  async deleteNoteByIdHandler(request) {
+    const { id } = request.params;
+    await this._service.deleteNoteById(id);
+
+    return {
+      status: 'Succes',
+      message: 'Catatan berhasil dihapus',
+    };
+  }
+}
+
+module.exports = NotesHandler;
