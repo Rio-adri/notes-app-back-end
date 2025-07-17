0d034c80e05d9810ac18cc730316b88e0208059c
Rio Adrian <rioadriansky02@gmail.com>
2025-07-17 13:56:03 +0700

    users registration implemented

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
-const notes = require('./api/notes/index');
+
+// notes
+const notesPlugin = require('./api/notes/index');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+
+// users
+const usersPlugin = require('./api/users/index');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users/index');
+
 const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -20,8 +29,12 @@ const init = async () => {
 
   await server.register([
     {
-      plugin: notes,
+      plugin: notesPlugin,
       options: { service: notesService, validator: NotesValidator },
+    },
+    {
+      plugin: usersPlugin,
+      options: { service: usersService, validator: UsersValidator },
     },
   ]);
 
---
+++ src/validator/users/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { UsersSchema } = require('./schema');
+
+const UsersValidator = {
+    validateUserPayload: (payload) => {
+        const result = UsersSchema.validate(payload);
+
+        if (result.error) {
+            throw new InvariantError(result.error.message);
+        }
+    }
+}
+
+module.exports = UsersValidator;
