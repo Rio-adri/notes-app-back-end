603b7f920cb440745615ebac3ed6bf3e20748703
Rio Adrian <rioadriansky02@gmail.com>
2025-07-18 13:22:42 +0700

    Authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 
 // notes
 const notesPlugin = require('./api/notes/index');
@@ -12,11 +13,18 @@ const usersPlugin = require('./api/users/index');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users/index');
 
+// authentications
+const authenticationPlugin = require('./api/authentications/index');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const authenticationsValidator = require('./validator/authentications/index');
+
 const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -27,6 +35,31 @@ const init = async () => {
     },
   });
 
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    }
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
+
+  // registrasi plugin internal
   await server.register([
     {
       plugin: notesPlugin,
@@ -36,6 +69,10 @@ const init = async () => {
       plugin: usersPlugin,
       options: { service: usersService, validator: UsersValidator },
     },
+    {
+      plugin: authenticationPlugin,
+      options: {service: {authenticationsService, usersService }, tokenManager: TokenManager, validator: authenticationsValidator}
+    }
   ]);
 
   server.ext('onPreResponse', (request, h) => {
